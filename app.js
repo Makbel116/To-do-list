297e41862fff1283c6ba1fbdd56759619228cfa0
unknown <makbel9395@gmail.com>
2024-02-28 05:57:06 -0800

    adds h1 to display the current day & a list showing the added tasks into the do list

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app=express();
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+let tasks=[];
+app.get("/", function (req, res) {
+    //to set the day 
+  let today = new Date();
+  let options = {
+      day: "numeric",
+      month: "long",
+      weekday: "long",
+    };
+  let day=today.toLocaleDateString("en-US", options);
+ 
+  // to render the list.ejs
+  res.render("list", { dateOfTheDay: day,toDoItem:tasks});
+});
 
-
-app.get("/",function(req,res){
-    
+app.post("/",function(req,res){
+    let task=req.body.addList;
+    tasks.push(task);
+    res.redirect("/")
 })
-app.listen(3000,function(req,res){
-    console.log("server is running.")
-})
+app.listen(3000, function (req, res) {
+  console.log("server is running.");
+});
